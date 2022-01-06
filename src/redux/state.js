c1fa6060c917b89f9aec546d948da3335883b7c7
XAOC615 <xaoc615@gmail.com>
2022-01-06 18:14:31 +0300

    refact

---
+++ src/redux/state.js
@@ -0,0 +1,31 @@
+ let state ={
+     profilePage : {
+        posts : [
+            { id: 1, message: 'hello bro', likes:'153'},
+            { id: 2, message: 'How are you?', likes:'204'},
+            { id: 3, message: 'Good job', likes:'253'},
+            { id: 4, message: 'I like you', likes:'187'},
+          ],
+     },
+      messagePage : {
+        messages : [
+            { id: 1, message: 'HI'},
+            { id: 2, message: 'How are you?'},
+            { id: 3, message: 'YO'},
+            { id: 4, message: 'YO'},
+            { id: 5, message: 'YO'},
+          ],
+          dialogs : [
+            { id: 1, name: 'Andrey'},
+            { id: 2, name: 'Sergey'},
+            { id: 3, name: 'Sveta'},
+            { id: 4, name: 'Valera'},
+            { id: 5, name: 'Dimych'},
+            { id: 6, name: 'Anna'},
+            { id: 7, name: 'Vasya'},
+            { id: 8, name: 'Petya'},
+          ],
+      }
+     
+  };
+  export default state;
